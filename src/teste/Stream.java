21138924ce5d1a82d61cc4081eaf5abb0fc87b60
Rodrigo Moreira Carvalho <rodrigo_moreira84@hotmail.com>
2019-11-07 17:25:22 -0300

    realizando exercicios

--- src/teste/Stream.java
+++ src/teste/Stream.java
@@ -1,10 +1,12 @@
 package teste;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class Stream {
@@ -33,8 +35,9 @@ public class Stream {
 		System.out.println("Maior: " + statistics.getMax());
 		System.out.println("Menor: " + statistics.getMin());
 		
-		List<Pessoa> listPessoas = pessoas.stream().filter(p -> p.getNome().startsWith("B"))
-			.collect(Collectors.toList());
+		List<Pessoa> listPessoas = pessoas.stream()
+				.filter(p -> p.getNome().startsWith("B"))
+				.collect(Collectors.toList());
 		
 		Map<Integer, List<Pessoa>> mapPessoas = listPessoas.stream()
 				.collect(Collectors.groupingBy(Pessoa::getIdade));
@@ -55,16 +58,27 @@ public class Stream {
 			System.out.println(optPessoal.get().getIdade());
 		}
 		
-		
 		optPessoal.ifPresent(p -> System.out.println(p.getIdade()));
 		
+		System.out.println("Lista de números");
+		List<Integer>listNum = Arrays.asList(5, 9, 4, 16, 2, 21, 3);
+		listNum.forEach(System.out::println);
 		
+		System.out.println("Lista de números ordenados");
+		List<Integer> listSorted = listNum.stream()
+				.sorted()
+				.collect(Collectors.toList());
+		listSorted.forEach(System.out::println);
 		
+		pessoas.forEach(System.out::println);
+		System.out.println("Lista de pessoas ordenados por idade");
 		
+		java.util.stream.Stream<Pessoa> pessoasSorted = pessoas.stream()
+				.sorted(Comparator.comparingInt(Pessoa::getIdade));
+		java.util.stream.Stream<Pessoa> pessoasSortedInverso = pessoas.stream()
+				.sorted(Comparator.comparingInt(Pessoa::getIdade).reversed());
+		pessoasSorted.forEach(System.out::println);
 		
-		
-		
-
 	}
 
 }
