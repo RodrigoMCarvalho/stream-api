38fe2e4b61b2938e6018e9895af438d44909e53e
Rodrigo <rodrigo_moreira84@hotmail.com>
2022-04-07 10:25:53 -0300

    testes

--- src/com/dev/reflection/ReflectionTest.java
+++ src/com/dev/reflection/ReflectionTest.java
@@ -1,15 +1,33 @@
 package com.dev.reflection;
 
 import com.dev.models.Motorista;
+import com.dev.models.Pessoa;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class ReflectionTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException {
+
+        Class<?> classe = Class.forName(Motorista.class.getName());
+        System.out.println("Caminho da classe: " + classe);
+        System.out.println("Nome da classe: " + classe.getSimpleName());
+
+        Constructor<?>[] construtores = classe.getDeclaredConstructors();
+        for (Constructor<?> construtor : construtores) {
+            System.out.println("Construtor: " + construtor);
+        }
+
+        Method[] metodos = classe.getDeclaredMethods();
+
+        for (Method metodo : metodos) {
+            System.out.println("Metodo: " + metodo);
+        }
 
         List<String> nomesAtributos = Arrays.stream(Motorista.class.getDeclaredFields()).map(Field::getName).collect(Collectors.toList());
 
@@ -18,5 +36,12 @@ public class ReflectionTest {
                 .collect(Collectors.toList());
 
         nomesPrimeiraLetraMaiuscula.forEach(System.out::println);
+
+        Pessoa pessoa = new Pessoa("Rodrigo", 20);
+        Class<?> classePessoa = Class.forName(Pessoa.class.getName());
+        Field nome = classePessoa.getDeclaredField("nome");
+        nome.setAccessible(true);  //Devido ao atributo nome ser privado
+        nome.set(pessoa, "Gustavo");
+        System.out.println("Novo valor do atributo: " + pessoa);
     }
 }
